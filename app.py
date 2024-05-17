b0b0d23206f5d0dd30801515e69daef217e26845
Azay961 <ajay023.chaudhary@gmail.com>
2024-05-17 23:45:15 +0545

    Update

--- app.py
+++ app.py
@@ -1,63 +1,46 @@
-import os
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model, model_from_json
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from tensorflow.keras.preprocessing import image
-# model = model_from_json(open("static\model_arch.json", "r").read())
-# model.load_weights('static\model.h5')
-model = load_model('D:/my_projects/Facial emotion detection/CodeClause_Facial-Emotions-Detection/models/model_filter.h5')
-# model = load_model('static\Fer2013.h5')
-face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-cap=cv2.VideoCapture(0)
+face_classifier = cv2.CascadeClassifier(r'D:\my_projects\Facial emotion detection\CodeClause_Facial-Emotions-Detection\haarcascade_frontalface_default.xml')
+classifier =load_model(r'D:\my_projects\Facial emotion detection\CodeClause_Facial-Emotions-Detection\models\model_filter.keras')
 
-while cap.isOpened():
-    res,frame=cap.read()
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
-    height, width , channel = frame.shape
-    sub_img = frame[0:int(height/6),0:int(width)]
-
-    black_rect = np.ones(sub_img.shape, dtype=np.uint8)*0
-    res = cv2.addWeighted(sub_img, 0.77, black_rect,0.23, 0)
-    FONT = cv2.FONT_HERSHEY_SIMPLEX
-    FONT_SCALE = 0.8
-    FONT_THICKNESS = 2
-    lable_color = (10, 10, 255)
-    lable = "Emotion Detection"
-    lable_dimension = cv2.getTextSize(lable,FONT ,FONT_SCALE,FONT_THICKNESS)[0]
-    textX = int((res.shape[1] - lable_dimension[0]) / 2)
-    textY = int((res.shape[0] + lable_dimension[1]) / 2)
-    cv2.putText(res, lable, (textX,textY), FONT, FONT_SCALE, (0,0,0), FONT_THICKNESS)
-    gray_image= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_haar_cascade.detectMultiScale(gray_image )
-    try:
-        for (x,y, w, h) in faces:
-            cv2.rectangle(frame, pt1 = (x,y),pt2 = (x+w, y+h), color = (255,0,0),thickness =  2)
-            roi_gray = gray_image[y-5:y+h+5,x-5:x+w+5]
-            roi_gray=cv2.resize(roi_gray,(48,48))
-            image_pixels = img_to_array(roi_gray)
-            image_pixels = np.expand_dims(image_pixels, axis = 0)
-            image_pixels /= 255
-            predictions = model.predict(image_pixels)
-            max_index = np.argmax(predictions[0])
-            emotion_detection = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-            emotion_prediction = emotion_detection[max_index]
-            cv2.putText(res, "Sentiment: {}".format(emotion_prediction), (0,textY+22+5), FONT,0.7, lable_color,2)
-            lable_violation = 'Confidence: {}'.format(str(np.round(np.max(predictions[0])*100,1))+ "%")
-            violation_text_dimension = cv2.getTextSize(lable_violation,FONT,FONT_SCALE,FONT_THICKNESS )[0]
-            violation_x_axis = int(res.shape[1]- violation_text_dimension[0])
-            cv2.putText(res, lable_violation, (violation_x_axis,textY+22+5), FONT,0.7, lable_color,2)
-    except :
-        pass
-    frame[0:int(height/6),0:int(width)] =res
-    cv2.imshow('frame', frame)
+cap = cv2.VideoCapture(0)
 
 
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.imshow('Emotion Detector',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
-
 cap.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
